44d66b17551880042c4c90064cac00a53c14b4f8
Jescipe2025 <jescipe@gmail.com>
2026-05-05 12:20:21 -0300

    Update usuarios.py

--- usuarios.py
+++ usuarios.py
@@ -1,6 +1,6 @@
 USUARIOS = {
     "Luciana" : {"senha": "PlPb@356", "paineis":    ["estoque"]},
     "Thamires": {"senha": "PlPb@984", "paineis":    ["compras"]},
-    "Claudete": {"senha": "PlPb@956", "paineis":    ["financieiro"]},
+    "Claudete": {"senha": "PlPb@956", "paineis":    ["financeiro"]},
     "admin":    {"senha": "J3sc1p3@2026", "paineis": ["financeiro", "estoque", "compras"]},
 }
